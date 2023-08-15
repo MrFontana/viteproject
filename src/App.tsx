b44a47c675e6ea9f731e1cc77b73bf63e3e608c2
CCKevinFontana <kfontana@crescendocollective.com>
2023-08-15 16:35:16 -0700

    Added dog list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React changes</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <DogList />
+    </div>
+  );
 }
 
-export default App
+function DogList() {
+  const [dogs, setDogs] = useState([]);
+  const [newDog, setNewDog] = useState('');
+
+  const addDog = () => {
+    if (newDog.trim() !== '') {
+      setDogs([...dogs, newDog]);
+      setNewDog('');
+    }
+  };
+
+  const removeDog = (index) => {
+    const updatedDogList = dogs.filter((_, i) => i !== index);
+    setDogs(updatedDogList);
+  };
+
+  return (
+    <div className="dogsBaybay">
+      <h1>List of Dogs</h1>
+      <div>
+        <input
+          type="text"
+          value={newDog}
+          onChange={(e) => setNewDog(e.target.value)}
+          placeholder="Enter a new task"
+        />
+        <button onClick={addDog}>Add Dog</button>
+      </div>
+      <ul>
+        {dogs.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeDog(index)}>Remove Dog</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
